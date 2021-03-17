dba1791c3232199066ffeb62be4d425a35f434a1
Pratik_Bhangale <pratiks.bhangale@gmail.com>
2021-03-17 12:44:15 +0530

    Commit 3

---
+++ BIRCHClustering.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+from scipy import stats
+from sklearn.metrics import silhouette_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans, Birch
+from sklearn.cluster import AffinityPropagation
+from sklearn.cluster import AgglomerativeClustering
+import seaborn as sns
+import scipy.cluster.hierarchy as sch
+
+df = pd.read_excel("Online Retail.xlsx")
+df = df[df['CustomerID'].notna()]
+print(df.shape)
+
+# For Sampling the dataset
+df_sampled = df.sample(300000)
+df_sampled["InvoiceDate"] = df_sampled["InvoiceDate"].dt.date
+print(df_sampled.shape)
+
+# Adding the Total sum column
+df_sampled['TotalSum'] = df_sampled['Quantity'] * df_sampled['UnitPrice']
+print(df_sampled.shape)
+
+# For Calculating Recency
+snapshot_date = max(df_sampled.InvoiceDate) + datetime.timedelta(days=1)
+
+# Aggregate data of each Individual Customer
+customers = df_sampled.groupby(['CustomerID']).agg({
+    'InvoiceDate': lambda x: (snapshot_date - x.max()).days,
+    'InvoiceNo': 'count',
+    'TotalSum': 'sum'})
+
+# Renaming the columns according to rfm table
+customers.rename(columns={'InvoiceDate': 'Recency',
+                          'InvoiceNo': 'Frequency',
+                          'TotalSum': 'MonetaryValue'},
+                 inplace=True)
+
+# Reducing the Scewness of the data
+customers_fix = pd.DataFrame()
+customers_fix["Recency"] = stats.boxcox(customers['Recency'])[0]
+customers_fix["Frequency"] = stats.boxcox(customers['Frequency'])[0]
+customers_fix["MonetaryValue"] = pd.Series(np.cbrt(customers['MonetaryValue'])).values
+customers_fix.tail()
+
+# Normalising the Dataset
+scaler = StandardScaler()
+scaler.fit(customers_fix)
+customers_normalized = scaler.transform(customers_fix)
+
+# Assert that the dataset has mean 0 and variance 1
+print(customers_normalized.mean(axis=0).round(2))  # [0. -0. 0.]
+print(customers_normalized.std(axis=0).round(2))  # [1. 1. 1.]
+
+
+# BIRCH Clustering
+
+model = Birch(branching_factor=50, n_clusters=None, threshold=1.5)
+birch = model.fit_predict(customers_normalized)
+plt.figure(figsize=(8, 8))
+plt.scatter(customers_normalized[birch == 0, 0], customers_normalized[birch == 0, 1], s=10, c='red')
+plt.scatter(customers_normalized[birch == 1, 0], customers_normalized[birch == 1, 1], s=10, c='blue')
+plt.scatter(customers_normalized[birch == 2, 0], customers_normalized[birch == 2, 1], s=10, c='green')
+plt.title('Clusters of customers using Incremental Clustering')
+plt.show()
